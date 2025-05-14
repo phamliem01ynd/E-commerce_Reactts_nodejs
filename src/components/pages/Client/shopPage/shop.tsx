8e30989fac8386f409314a7673b564123f5d4c38
phamliem01ynd <phamliem01ynd@gmail.com>
2025-05-14 22:43:25 +0700

    update: feature/filter

--- src/components/pages/Client/shopPage/shop.tsx
+++ src/components/pages/Client/shopPage/shop.tsx
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Category } from "../../../../models/Category";
 import { Product } from "../../../../models/Product";
 import { getCategoriesAll } from "../../../../core/apis/categoriesService";
 import { paginationProduct } from "../../../../core/apis/productService";
 import { Link, useSearchParams } from "react-router-dom";
-import { Grid, MenuItem, Select } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 import "./shop.scss";
 import ProductShop from "../../../common/productShop/productShop";
+import { TranslateService } from "../../../../core/services/translateService";
 
 function Shop() {
   const [categories, setCategories] = useState<Category[]>([]);
   const [products, setProducts] = useState<Product[]>([]);
   const [quantityPage, setQuantityPage] = useState<number>(1);
   const [pageParams, setPageParams] = useSearchParams();
-
+  const { translates } = useContext(TranslateService);
   useEffect(() => {
     const page = pageParams.get("page") || "1";
     const fetchApi = async () => {
@@ -27,16 +35,14 @@ function Shop() {
         setCategories(result2.data);
       }
     };
-    fetchApi(); 
+    fetchApi();
   }, [pageParams]);
   console.log("prodyctPage: ", products);
   console.log("category: ", categories);
 
   const [categoryProduct, setCategoryProduct] = useState<number | null>(null);
   const [sortPrice, setSortPrice] = useState<string | null>(null);
-  const handleClickCategory = (id: number) => {
-    setCategoryProduct(id);
-  };
+
   const handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSortPrice(event.target.value);
   };
@@ -62,6 +68,14 @@ function Shop() {
     setCategoryProduct(null);
   };
 
+  const handleChangeCategory = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setCategoryProduct(event.target.value);
+  };
+
+  console.log("categoryid: ", categoryProduct);
+
   console.log("SortProduct: ", products);
   return (
     <div>
@@ -69,38 +83,80 @@ function Shop() {
         <Link to={"/"}>Home</Link>/ Shop
       </div>
       <div className="shop">
-        <Grid container>
+        <Grid container spacing={6}>
           <Grid item xl={2} lg={2} md={4} sm={4} xs={4}>
             <div className="categoryFilter">
-              <h2>Danh mục sản phẩm</h2>
-              {categories
-                ? categories.map((item) => (
-                    <div
-                      className="category__list"
-                      key={item.id}
-                      onClick={() => handleClickCategory(item.id)}
-                    >
-                      <span>{item.name}</span>
-                    </div>
-                  ))
-                : "Không tìm thấy sản phẩm"}
-            </div>
-            <div className="productFilter">
-              <h2>Lọc sản phẩm</h2>
-              <div className="allProduct" onClick={handleShowAllProduct}>
-                <span>Tất cả sản phẩm</span>
-              </div>
-              <div className="sortPrice">
-                <span className="price">Giá sản phẩm</span>
-                <Select
-                  defaultValue={"Giá sản phẩm"}
-                  style={{ width: 250 }}
-                  onChange={handleOnChange}
+              <h3>{translates.filterProduct}</h3>
+              <div className="productall">
+                <Button
+                  onClick={handleShowAllProduct}
+                  style={{
+                    width: "160px",
+                    height: "30px",
+                    borderRadius: "15px",
+                    margin: "12px",
+                    background: "#fafa",
+                  }}
                 >
-                  <MenuItem value="Giá tăng dần">Giá tăng dần</MenuItem>
-                  <MenuItem value="Giá giảm dần">Giá giảm dần</MenuItem>
-                </Select>
+                  {translates.ProductAll}
+                </Button>
               </div>
+              {categories ? (
+                <>
+                  <FormControl variant="outlined" className="category">
+                    <InputLabel id="category">{translates.category}</InputLabel>
+                    <Select
+                      labelId="city"
+                      label="City"
+                      className="select"
+                      style={{ width: "160px", height: "30px" }}
+                      onChange={handleChangeCategory}
+                      MenuProps={{
+                        disableScrollLock: true,
+                        PaperProps: {
+                          style: {
+                            maxHeight: 150,
+                            overflowY: "auto",
+                          },
+                        },
+                      }}
+                    >
+                      {categories.map((item) => (
+                        <MenuItem key={item.id} value={item.id}>
+                          {item.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </>
+              ) : (
+                <p>Không tìm thấy danh mục</p>
+              )}
+              <div>
+                <FormControl variant="outlined" className="sort">
+                  <InputLabel id="sort">{translates.Sort}</InputLabel>
+                  <Select
+                    labelId="city"
+                    label="City"
+                    className="select"
+                    style={{ width: "160px", height: "30px" }}
+                    onChange={handleOnChange}
+                    MenuProps={{
+                      disableScrollLock: true,
+                      PaperProps: {
+                        style: {
+                          maxHeight: 150,
+                          overflowY: "auto",
+                        },
+                      },
+                    }}
+                  >
+                    <MenuItem value="Giá tăng dần">Giá tăng dần</MenuItem>
+                    <MenuItem value="Giá giảm dần">Giá giảm dần</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              ;
             </div>
           </Grid>
           <Grid item xl={10} lg={10} md={8} sm={8} xs={8}>
